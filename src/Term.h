6e55afe44b7fcd47f261d52f210bf09e6f864ccf
Matheus <msousa@sjtu.edu.cn>
2024-05-13 20:51:47 +0800

    Introduce density_density Term

--- src/Term.h
+++ src/Term.h
@@ -89,6 +89,18 @@ class Term {
                                       Operator::annihilation(spin2, orbital2)});
   }
 
+  static Term density_density(double coefficient, Operator::Spin spin1,
+                       std::uint8_t orbital1, Operator::Spin spin2,
+                       std::uint8_t orbital2, Operator::Spin spin3,
+                       std::uint8_t orbital3, Operator::Spin spin4,
+                       std::uint8_t orbital4) {
+    return Term(coefficient,
+                std::vector<Operator>{Operator::creation(spin1, orbital1),
+                                      Operator::annihilation(spin2, orbital2),
+                                      Operator::creation(spin3, orbital3),
+                                      Operator::annihilation(spin4, orbital4)});
+  }
+
   template <typename... Args>
   static Term term(double coefficient, Args... args) {
     std::vector<Operator> operators{args...};
