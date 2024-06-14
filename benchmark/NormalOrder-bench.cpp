88903e8137b6fe5f68ea1f1caeeee7e4595e3370
Matheus <msousa@sjtu.edu.cn>
2024-06-14 19:34:20 +0800

    Benchmark case were normal order takes infinite time

--- benchmark/NormalOrder-bench.cpp
+++ benchmark/NormalOrder-bench.cpp
@@ -8,6 +8,8 @@
 using enum Operator::Statistics;
 using enum Operator::Spin;
 
+static constexpr std::size_t max_orbital = 32;
+
 static void BM_NormalOrderTermEasy(benchmark::State& state) {
   for (auto _ : state) {
     state.PauseTiming();
@@ -16,10 +18,11 @@ static void BM_NormalOrderTermEasy(benchmark::State& state) {
     const int size = state.range(0);
     operators.reserve(size);
     for (int i = 0; i < size / 2; i++) {
-      operators.push_back(Operator::creation<Fermion>(Up, i));
+      operators.push_back(Operator::creation<Fermion>(Up, i % max_orbital));
     }
     for (int i = 0; i < size / 2; i++) {
-      operators.push_back(Operator::annihilation<Fermion>(Up, size - i + 1));
+      operators.push_back(Operator::annihilation<Fermion>(
+          Up, (size / 2 - i + 1) % max_orbital));
     }
     Term term(1.0, operators);
 
@@ -40,7 +43,8 @@ static void BM_NormalOrderTermHard(benchmark::State& state) {
     operators.reserve(size);
 
     for (int i = 0; i < size; i++) {
-      operators.push_back(Operator::annihilation<Fermion>(Up, size - i + 1));
+      operators.push_back(Operator::annihilation<Fermion>(
+          Up, (size / 2 - i + 1) % max_orbital));
     }
     operators.push_back(Operator::creation<Fermion>(Up, 0));
     Term term(1.0, operators);
@@ -53,7 +57,7 @@ static void BM_NormalOrderTermHard(benchmark::State& state) {
 
 BENCHMARK(BM_NormalOrderTermHard)->Range(8, 8 << 10);
 
-static void BM_NormalOrderTermHarder(benchmark::State& state) {
+static void BM_NormalOrderTermHarderEasyVersion(benchmark::State& state) {
   for (auto _ : state) {
     state.PauseTiming();
 
@@ -62,10 +66,10 @@ static void BM_NormalOrderTermHarder(benchmark::State& state) {
     operators.reserve(size);
 
     for (int i = 0; i < size / 2; i++) {
-      operators.push_back(Operator::annihilation<Fermion>(Up, size - i + 1));
+      operators.push_back(Operator::annihilation<Fermion>(Up, 1));
     }
     for (int i = 0; i < size / 2; i++) {
-      operators.push_back(Operator::creation<Fermion>(Up, i));
+      operators.push_back(Operator::creation<Fermion>(Up, 0));
     }
     Term term(1.0, operators);
 
@@ -75,4 +79,53 @@ static void BM_NormalOrderTermHarder(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_NormalOrderTermHarder)->Range(8, 8 << 10);
+BENCHMARK(BM_NormalOrderTermHarderEasyVersion)->Range(8, 8 << 10);
+
+static void BM_NormalOrderTermHarder1(benchmark::State& state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+
+    std::vector<Operator> operators;
+    const int size = state.range(0);
+    operators.reserve(size);
+
+    for (int i = 0; i < size / 2; i++) {
+      operators.push_back(Operator::annihilation<Fermion>(Up, 0));
+    }
+    for (int i = 0; i < size / 2; i++) {
+      operators.push_back(Operator::creation<Fermion>(Up, 0));
+    }
+    Term term(1.0, operators);
+
+    state.ResumeTiming();
+    Expression e = normal_order(term);
+    benchmark::DoNotOptimize(e);
+  }
+}
+
+BENCHMARK(BM_NormalOrderTermHarder1)->RangeMultiplier(2)->Range(8, 32);
+
+static void BM_NormalOrderTermHarder2(benchmark::State& state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+
+    std::vector<Operator> operators;
+    const int size = state.range(0);
+    operators.reserve(size);
+
+    for (int i = 0; i < size / 2; i++) {
+      operators.push_back(Operator::annihilation<Fermion>(
+          Up, (size / 2 - i + 1) % max_orbital));
+    }
+    for (int i = 0; i < size / 2; i++) {
+      operators.push_back(Operator::creation<Fermion>(Up, i % max_orbital));
+    }
+    Term term(1.0, operators);
+
+    state.ResumeTiming();
+    Expression e = normal_order(term);
+    benchmark::DoNotOptimize(e);
+  }
+}
+
+BENCHMARK(BM_NormalOrderTermHarder2)->RangeMultiplier(2)->Range(8, 64);
