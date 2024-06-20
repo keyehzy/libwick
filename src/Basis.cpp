bdcc438c7fe6c325fa404cdf5f97881fa53febf0
Matheus <msousa@sjtu.edu.cn>
2024-06-20 15:30:17 +0800

    Reshuffle functions

--- src/Basis.cpp
+++ src/Basis.cpp
@@ -18,6 +18,46 @@ void prepare_up_and_down_representation(
   }
 }
 
+static constexpr std::string_view unicode_empty_cell = "  ";
+static constexpr std::string_view unicode_up_arrow_cell = "\u2191 ";
+static constexpr std::string_view unicode_down_arrow_cell = " \u2193";
+static constexpr std::string_view unicode_double_occ_cell = "\u2191\u2193";
+
+std::string Basis::state_string(const BasisElement& element) const {
+  LIBMB_ASSERT(contains(element));
+  std::vector<int> up(m_orbitals, 0);
+  std::vector<int> down(m_orbitals, 0);
+  prepare_up_and_down_representation(element, up, down);
+
+  std::stringstream out;
+  out << "|";
+
+  for (std::size_t i = 0; i < m_orbitals; i++) {
+    switch (2 * up[i] + down[i]) {
+      case 0:
+        out << unicode_empty_cell;
+        break;
+      case 1:
+        out << unicode_down_arrow_cell;
+        break;
+      case 2:
+        out << unicode_up_arrow_cell;
+        break;
+      case 3:
+        out << unicode_double_occ_cell;
+        break;
+      default:
+        LIBMB_UNREACHABLE();
+        break;
+    }
+    if (i < m_orbitals - 1) {
+      out << ",";
+    }
+  }
+  out << ">";
+  return out.str();
+}
+
 void Basis::generate_combinations(
     BasisElement& current, size_t first_orbital, size_t depth,
     size_t max_depth) {
@@ -49,44 +89,4 @@ void Basis::generate_basis() {
   BasisElement current;
   current.reserve(m_particles);
   generate_combinations(current, 0, 0, m_particles);
-}
-
-static constexpr std::string_view unicode_empty_cell = "  ";
-static constexpr std::string_view unicode_up_arrow_cell = "\u2191 ";
-static constexpr std::string_view unicode_down_arrow_cell = " \u2193";
-static constexpr std::string_view unicode_double_occ_cell = "\u2191\u2193";
-
-std::string Basis::state_string(const BasisElement& element) const {
-  LIBMB_ASSERT(contains(element));
-  std::vector<int> up(m_orbitals, 0);
-  std::vector<int> down(m_orbitals, 0);
-  prepare_up_and_down_representation(element, up, down);
-
-  std::stringstream out;
-  out << "|";
-
-  for (std::size_t i = 0; i < m_orbitals; i++) {
-    switch (up[i] * 2 + down[i]) {
-      case 0:
-        out << unicode_empty_cell;
-        break;
-      case 1:
-        out << unicode_down_arrow_cell;
-        break;
-      case 2:
-        out << unicode_up_arrow_cell;
-        break;
-      case 3:
-        out << unicode_double_occ_cell;
-        break;
-      default:
-        LIBMB_UNREACHABLE();
-        break;
-    }
-    if (i < m_orbitals - 1) {
-      out << ",";
-    }
-  }
-  out << ">";
-  return out.str();
 }
